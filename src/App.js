f343dce54592450a1adbfca3748893bae1f61633
rafa133169 <127886555+rafa133169@users.noreply.github.com>
2023-11-13 08:38:53 -0500

    corrigiendo Rutas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import MedicationChart from './pages/Medicamentos';
-import API from './pages/Pruebas';
+import MedicationChart from './Pages/Medicamentos';
+import API from './Pages/Pruebas';
 import Manana from './Components/Manana';
 import Tarde from './Components/Tarde';
-import Final from './pages/MedicamentosFinal';
+import Final from './Pages/MedicamentosFinal';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 
 function App() {
